83881e7f42d127fe67ce2ce4cf64bc2cbdaec5e4
Aron Griffis <aron@arongriffis.com>
2016-04-26 17:56:47 -0400

    Bump version to 0.1.5

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='vcrpy-unittest',
-    version='0.1.4',
+    version='0.1.5',
     description='Python unittest integration for vcr.py',
     long_description=long_description,
     url='https://github.com/agriffis/vcrpy-unittest',
